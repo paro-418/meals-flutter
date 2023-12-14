da7716e8de9ef2cc0eabdbbf959a459950b7e625
Praveen kumar verma <vermapraween6543@gmail.com>
2023-12-14 12:15:50 +0530

    added animation on meal photos

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -32,19 +32,30 @@ class MealDetailsScreen extends ConsumerWidget {
                   ),
                 ));
               },
-              icon: Icon(isFavorite ? Icons.star : Icons.star_border))
+              icon: AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 300),
+                  transitionBuilder: (child, animation) => RotationTransition(
+                        turns: Tween(begin: 0.9, end: 1.0).animate(animation),
+                        child: child,
+                      ),
+                  child: Icon(
+                    isFavorite ? Icons.star : Icons.star_border,
+                    key: ValueKey(isFavorite),
+                  )))
         ],
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.only(bottom: 8),
         child: Column(
           children: [
-            Image.network(
-              meal.imageUrl,
-              width: double.infinity,
-              height: 300,
-              fit: BoxFit.cover,
-            ),
+            Hero(
+                tag: meal.id,
+                child: Image.network(
+                  meal.imageUrl,
+                  width: double.infinity,
+                  height: 300,
+                  fit: BoxFit.cover,
+                )),
             const SizedBox(
               height: 14,
             ),
